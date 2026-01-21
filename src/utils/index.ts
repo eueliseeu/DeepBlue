f88fb4697b7991622ed51d66fdbd3aa5dcbd9e10
Eliseu Pereira <eueliseeu@gmail.com>
2026-01-21 06:40:09 -0300

    refactor: reorganize exports with new utility modules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,10 @@
-export { writeDockerFiles, fileExists, ensureDirectory } from "./file-handler";
+export {
+  writeDockerFiles,
+  writeDockerIgnore,
+  fileExists,
+  ensureDirectory,
+} from "./file-handler";
 export { isValidPort, normalizePort, isValidVersion } from "./validators";
+export { detectInstalledVersion } from "./version-detector";
+export { detectProject } from "./project-detector";
+export { generateDockerIgnore } from "./dockerignore-generator";
